3cf2e196154b8b696284d4d72a867ca678d39017
adrajagopal <adrajagopal@gmail.com>
2023-12-14 13:23:53 -0600

    configure google fonts, site.css, nuxt3-leaflet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import {fileURLToPath} from 'url';
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+	devtools: { enabled: true },
+	modules: [
+    ['@nuxtjs/google-fonts', {
+        families: {
+         'Inter': {
+            wght: [100, 300, 400, 500, 600, 700],
+            ital: '300..500',
+          }
+        }
+    }],
+    'nuxt3-leaflet',
+  ],
+	css: ['~/assets/css/site.css'],
 })
