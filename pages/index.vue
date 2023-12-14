0f96ac89db819be85559f98edacd4985187d8f2c
adrajagopal <adrajagopal@gmail.com>
2023-12-14 13:25:02 -0600

    Refactor leaflet - Nuxt3 conventions - STILL BROKE

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,23 @@
 
 <template>
 	<main>
+		<div style="height:100vh; width:100vw">
+		    <LMap
+		      ref="map"
+		      :zoom="13"
+		      :center="[43.07485907699501, -89.38413717048415]"
+		    >
+			    <LTileLayer
+			      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+		        attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+		        layer-type="base"
+		        name="OpenStreetMap"
+		      />
+		      <LMarker
+		      	:lat-lng="[43.0924168968808, -89.35550384616374]"
+		      ></LMarker>
+		    </LMap>
+		</div>
 	</main>
 </template>
 
