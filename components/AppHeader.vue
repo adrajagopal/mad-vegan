9352744432c8f40464a58ea01653389dcea9d6bd
adrajagopal <adrajagopal@gmail.com>
2023-12-19 15:11:17 -0600

    Conditionally show header based on login

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,24 +1,36 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+const user = useSupabaseUser();
 
 </script>
 <template>
 	<inner-column>
-		<div class="site-header">
+		<div v-if="user" class="site-header">
 			<RouterLink to="/">
 				<picture class="logo">
 					<img alt="VM logo" src="@/assets/logo.svg" />
 				</picture>
 			</RouterLink>
-		
+			
 			<div class="wrapper" >
 				<nav>
 					<RouterLink to="/">Home</RouterLink>
 					<RouterLink to="/add-restaurant">Add restaurant</RouterLink>
 					<SignOut/>
 				</nav>
+				<p class="small-voice">User: {{user.email}}</p>
 			</div>
 		
+		</div>
+
+		<div v-else>
+			<RouterLink to="/">
+				<picture class="logo">
+					<img alt="VM logo" src="@/assets/logo.svg" />
+				</picture>
+			</RouterLink>
+
+			<RouterLink to="/about">About</RouterLink>
 		</div>
 	</inner-column>
 </template>
