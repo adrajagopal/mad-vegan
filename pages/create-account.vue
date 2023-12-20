ca339006faf2a11ceb6ea8588236ba88303b5869
adrajagopal <adrajagopal@gmail.com>
2023-12-19 20:38:00 -0600

    Flesh out create-account page

--- pages/create-account.vue
+++ pages/create-account.vue
@@ -2,8 +2,17 @@
 
 <template>
   <inner-column>
+    <h1 class="xxl-voice">Create account</h1>
+
     <CreateAccountForm/>
-  </inner-column>
+
+    <p>
+      Already have an account?
+      <NuxtLink to="/sign-in" class="medium-voice">
+        Sign in.
+      </NuxtLink>
+    </p>
+   </inner-column>
 </template>
 
 <style scoped></style>
