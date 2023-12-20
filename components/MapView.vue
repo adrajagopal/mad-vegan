d314ab014989e825a47fda5431549a2e52dc1e1c
adrajagopal <adrajagopal@gmail.com>
2023-12-20 13:30:49 -0600

    move map view to component
    move map

---
+++ components/MapView.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div id="map" style="height:100vh; width:100vw">
+      <LMap
+        ref="map"
+        :zoom="13"
+        :center="[43.07485907699501, -89.38413717048415]"
+      >
+        <LTileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+          layer-type="base"
+          name="OpenStreetMap"
+        />
+        <LMarker
+          :lat-lng="[43.0924168968808, -89.35550384616374]"
+        ></LMarker>
+      </LMap>
+  </div>
+</template>
+
+<style scoped></style>
