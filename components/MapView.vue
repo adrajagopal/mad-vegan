8fc7e30689206ffa82175545aeab7290aad426ce
adrajagopal <adrajagopal@gmail.com>
2023-12-26 23:39:22 -0500

    Customize marker icon

--- components/MapView.vue
+++ components/MapView.vue
@@ -3,7 +3,12 @@
 
 	const { data, error } = await supabase
 		.from('restaurants')
-		.select(`id, name, lat, long`);
+		.select(`id, name, lat, long, website, cuisine, has_vegan_menu, is_completely_vegan`);
+
+
+	let iconUrl = "https://ucxtdreexegztsldmyjv.supabase.co/storage/v1/object/public/media-library/pindrop-green-dark-gradient.svg?t=2023-12-27T04%3A36%3A59.120Z";
+	let iconSize = [34, 41];
+	let iconAnchor = [17, 41];
 
 </script>
 
@@ -24,6 +29,7 @@
 				<li v-for="place in data">
 					<LMarker :lat-lng="[place.lat, place.long]">
 						<LPopup>{{place.name}}</LPopup>
+						<LIcon :icon-url="iconUrl" :icon-size="iconSize" :icon-anchor="iconAnchor"/>
 					</LMarker>
 				</li>
 			</ul>
