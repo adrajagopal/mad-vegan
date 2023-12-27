ed0d63e54c7c6cc7a7d0c27f1ee3f8d0236cae87
adrajagopal <adrajagopal@gmail.com>
2023-12-26 21:08:13 -0500

    Loop through pin creation for each restaurant

--- components/MapView.vue
+++ components/MapView.vue
@@ -9,21 +9,23 @@
 
 <template>
 	<div id="map" style="height:100vh; width:100vw">
-			<LMap
-				ref="map"
-				:zoom="13"
-				:center="[43.09229276307855, -89.38413717048415]"
-			>
-				<LTileLayer
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-					attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-					layer-type="base"
-					name="OpenStreetMap"
-				/>
-				<LMarker
-					:lat-lng="[43.092244520961515, -89.3555199355821]"
-				></LMarker>
-			</LMap>
+		<LMap
+			ref="map"
+			:zoom="13"
+			:center="[43.09229276307855, -89.38413717048415]"
+		>
+			<LTileLayer
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+				layer-type="base"
+				name="OpenStreetMap"
+			/>
+			<ul>
+				<li v-for="place in data">
+					<LMarker :lat-lng="[place.lat, place.long]"></LMarker>
+				</li>
+			</ul>
+		</LMap>
 	</div>
 </template>
 
