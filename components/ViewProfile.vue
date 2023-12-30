50e0bf3132f47693facc5ab58e43cf1514298e7a
adrajagopal <adrajagopal@gmail.com>
2023-12-29 19:05:25 -0500

    Create ViewProfile component; render profile data

---
+++ components/ViewProfile.vue
@@ -0,0 +1,19 @@
+<script setup>
+	const supabase = useSupabaseClient()
+
+	const { data, error } = await supabase
+		.from('profiles')
+		.select(`user_id, handle, eats_meat, eats_dairy, diet_category, eats_eggs, avatar, likes_imitation_meat`);
+
+	const user = useUserStore();
+
+	const id = user.id;
+
+</script>
+
+<template>
+	{{data}}
+
+</template>
+
+<style scoped></style>
