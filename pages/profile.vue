7f649fee870550c2527aba72b214e6be6eebf66d
adrajagopal <adrajagopal@gmail.com>
2023-12-29 19:03:31 -0500

    create profile page

---
+++ pages/profile.vue
@@ -0,0 +1,17 @@
+<script setup>
+	
+</script>
+
+<template>
+	<inner-column>
+		<h1 class="xxl-voice">Profile</h1>
+
+		<EditProfile/>
+
+		<ViewProfile/>
+
+
+	</inner-column>
+</template>
+
+<style scoped></style>
