dcba89c72382e549a9e0379c5e7a6cff17c59144
adrajagopal <adrajagopal@gmail.com>
2023-12-30 12:56:22 -0500

    Create profile.js pinia store — NOT FUNCTIONAL

--- stores/profile.js
+++ stores/profile.js
@@ -1,16 +1,18 @@
 export const useProfileStore = defineStore('profile', () => {
+	const user = useUserStore();
+	const id = user.id;
 
-	const supabase = useSupabaseClient()
+	// const supabase = useSupabaseClient()
 
-	const { data, error } = await supabase
-		.from('profiles')
-		.select(`user_id, handle, eats_meat, eats_dairy, diet_category, eats_eggs, avatar, likes_imitation_meat`)
-		.eq('user_id', id);
+	// const { data, error } = await supabase
+	// 	.from('profiles')
+	// 	.select(`user_id, handle, eats_meat, eats_dairy, diet_category, eats_eggs, avatar, likes_imitation_meat`)
+	// 	.eq('user_id', id);
 
-	const avatar = ref(data.avatar);
+	// const avatar = ref(data.avatar);
 
 	return {
-		avatar
+		// avatar
 	}
 
 })
