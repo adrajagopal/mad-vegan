7ddd7d719597c14ef322fe546ad4ae86922e361c
adrajagopal <adrajagopal@gmail.com>
2024-02-08 17:02:04 -0600

    Create /design-system to test components

---
+++ pages/design-system.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+	<inner-column>
+		<form>
+			<InputCheckbox/>
+			<InputRadio/>
+			<InputText/>
+			<Button/>			
+
+		</form>
+
+
+
+
+	</inner-column>
+</template>
+
+<style scoped></style>
